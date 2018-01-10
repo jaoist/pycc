8622ce9ec866574c1633747984701c08c50e1b0e
JAO <jao@JAO-Macbook-Pro.local>
2018-01-10 18:19:42 -0500

    Added git functionality

---
+++ Dictionaries/6-1.py
@@ -0,0 +1,10 @@
+# Person: Record some info about a person in a dictionary.
+
+myfriend = {
+    'firstName': 'phil',
+    'lastName': 'regan',
+    'age': 34,
+    'city': 'louisville',
+    }
+
+print 
