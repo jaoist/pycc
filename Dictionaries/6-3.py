e2e093db04817f86cd1786a6250199e4b736996a
JAO <jao@JAO-Macbook-Pro.local>
2018-01-29 00:14:00 -0500

    Late night baby coding

---
+++ Dictionaries/6-3.py
@@ -0,0 +1,19 @@
+#Programming glossary
+
+glossary = {
+    'variable':'A unique declared location that is used to store values',
+    'dictionary':'A list of key/value pairs. Use a the key to access the value'
+    ' which can be of any type.',
+    'loop':'A type of program flow control. Keeps executing as long as '
+    'conditions permit.',
+    'function':'An encapsulation of code that can take arguments that performs'
+    ' a task and usually returns a value.',
+    'integer':'Whole numbers. 2,147,483,647 max positive value. Can be negative'
+    ,
+    }
+
+print ('Variable:\n' + glossary['variable'])
+print ('Dictionary:\n' + glossary['dictionary'])
+print ('Loop:\n' + glossary['loop'])
+print ('Function:\n' + glossary['function'])
+print ('Integer:\n' + glossary['integer'])
