a810d74fdb43dd322e5b9bba9a11a3067101d0b3
JAO <jao@JAO-Macbook-Pro.local>
2018-01-29 00:14:33 -0500

    Fuuuuccckkk

--- Dictionaries/6-2.py
+++ Dictionaries/6-2.py
@@ -5,5 +5,8 @@ favNums = {
     'harrison':1,
     'peanut':42,
     }
-
-    
+print ("Ken's favorite number is " + str(favNums['ken']))
+print ("Jacksons's favorite number is " + str(favNums['jackson']))
+print ("Tim's favorite number is " + str(favNums['tim']))
+print ("Harrison's favorite number is " + str(favNums['harrison']))
+print ("Peanut's favorite number is " + str(favNums['peanut']))
