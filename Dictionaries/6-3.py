a823e0895381aaca4ad5070b633b974300bbae0a
JAO <jao@JAO-Macbook-Pro.local>
2018-02-04 17:52:49 -0500

    Added new example

--- Dictionaries/6-3.py
+++ Dictionaries/6-3.py
@@ -1,7 +1,7 @@
 #Programming glossary
 
 glossary = {
-    'variable':'A unique declared location that is used to store values',
+    'variable':'A unique declared location that is used to store values.',
     'dictionary':'A list of key/value pairs. Use a the key to access the value'
     ' which can be of any type.',
     'loop':'A type of program flow control. Keeps executing as long as '
@@ -9,7 +9,7 @@ glossary = {
     'function':'An encapsulation of code that can take arguments that performs'
     ' a task and usually returns a value.',
     'integer':'Whole numbers. 2,147,483,647 max positive value. Can be negative'
-    ,
+    '.',
     }
 
 print ('Variable:\n' + glossary['variable'])
---
+++ Dictionaries/user.py
@@ -0,0 +1,12 @@
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+
+for key, value in user_0.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
+
+# The items() method, when used on a dictionary, returns a list of key-value
+# pairs.
